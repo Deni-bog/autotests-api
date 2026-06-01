5897c2574897de140218bad4bdde06aac21b31a1
Deni-bog <WeakErection@yandex.ru>
2026-06-01 20:20:02 +0300

    практика работы с httpp

--- httpx_authentication.py
+++ httpx_authentication.py
@@ -1,24 +1,38 @@
-import httpx
-
-
-login_payload = {
-  "email": "valera_old@mail.ru",
-  "password": "12345D"
-}
-
-login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json = login_payload)
-
-login_response_data = login_response.json()
-
-print("Login response: ", login_response_data)
-print("Status code: ", login_response.status_code)
-
-refresh_payload = {
-    "refreshToken": login_response_data["token"]["refreshToken"],
-
-}
-refresh_response=httpx.post("http://localhost:8000/api/v1/authentication/refresh", json = refresh_payload)
-refresh_response_data = refresh_response.json()
-
-print("Refresh response: ", refresh_response_data)
-print("Status code: ", refresh_response.status_code)
+# import json
+#
+# import httpx
+#
+#
+# login_payload = {
+#   "email": "valera_old@mail.ru",
+#   "password": "12345D"
+# }
+#
+# login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json = login_payload)
+#
+# login_response_data = login_response.json()
+#
+# print("Login response: ", login_response_data)
+# print("Status code: ", login_response.status_code)
+#
+# me_payload = {
+#     "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+#
+# }
+#
+# # print(me_payload)
+#
+# response_me = httpx.get("http://localhost:8000/api/v1/users/me",headers=me_payload)
+# print(response_me.status_code)
+# print(response_me)
+#
+#
+# # refresh_payload = {
+# #     "refreshToken": login_response_data["token"]["refreshToken"],
+# #
+# # }
+# # refresh_response=httpx.post("http://localhost:8000/api/v1/authentication/refresh", json = refresh_payload)
+# # refresh_response_data = refresh_response.json()
+# #
+# # print("Refresh response: ", refresh_response_data)
+# # print("Status code: ", refresh_response.status_code)
