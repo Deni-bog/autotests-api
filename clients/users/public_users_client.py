7f3738f21781145e8cb9d1059ed7875dc778ae55
Deni-bog <WeakErection@yandex.ru>
2026-06-04 21:15:10 +0300

    Практика в реализации API клиентов

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,20 @@
+from httpx import Response
+from clients.api_client import APIClient
+from typing import TypedDict
+
+class CreateUserRequestDict(TypedDict):
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+
+    def create_user_api(self,request:CreateUserRequestDict) -> Response:
+        """
+        Метод создает пользователя.
+        :param request: Словарь с email, password, lastName, firstName, middleName.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
